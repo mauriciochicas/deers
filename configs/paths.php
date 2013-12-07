eb06c6df3fd5c117b5563c2acc2fbc7f27cc262d
Mario Menjivar <mariomenjr@gmail.com>
2013-12-06 18:57:53 -0800

    Bootstrap created.
    The bootstrap has been created.
    It manage the url petitions.

---
+++ configs/paths.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* Mario Menjivar <mariomenjr@gmail.com>
+* @mariomenjr
+* Path constants.
+* Principal used paths in the apps.
+* Thursday 05, dic 2013
+*/
+
+//Principal Folders
+define('CORE', ROOT. 'core' . DS);
+define('CONTROLLERS', ROOT . 'controllers' . DS);
+define('MODELS', ROOT . 'models' . DS);
+define('VIEWS', ROOT. 'views' . DS);
+define('LAYOUT', VIEWS . 'layouts' . DS . 'default' . DS);
