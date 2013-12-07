184d296128707dd44f108014aa39a948479cf8b2
Mario Menjivar <mariomenjr@gmail.com>
2013-12-06 19:34:11 -0800

    All is cleaned now.
    Have been created Controller parent.

--- configs/config.php
+++ configs/config.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+* Mario Menjivar <mariomenjr@gmail.com>
+* @mariomenjr
+* General configurations.
+* This is used for all application, be careful.
+* Thursday 05, dic 2013
+*/
+
 $configFiles = array(
 		
 	);
---
+++ core/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* Mario Menjivar <mariomenjr@gmail.com>
+* @mariomenjr
+* Bootstrap class.
+* It is responsible of calling the correct controler depending of url.
+* Thursday 05, dic 2013
+*/
+class Controller {
+	
+	function __construct(argument) {
+		
+	}
+}
